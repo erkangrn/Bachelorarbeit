9504d24004e2824be0e163cbafd49ac3164893dd
erkangrn <erkangaran12345@gmail.com>
2023-03-15 11:45:26 +0100

    Präsi

---
+++ Code/Working/PredictSAR.bat
@@ -0,0 +1,4 @@
+CALL getTestData.bat
+@echo off
+"C:/Users/z0044ber/AppData/Local/Programs/Python/Python310/python.exe" "c:/Users/z0044ber/Desktop/Ostfalia/Bachelorarbeit/Bachelorarbeit/Code/Working/Bsc_AI_Modell.py"
+pause
